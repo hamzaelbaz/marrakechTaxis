531abc028332a79536f4ec8d4cca68d387b085d4
hamza <elbazhamza77@gmail.com>
2024-11-03 14:23:09 +0100

    package json changing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fetch = require('node-fetch').default;
+const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 3000;
 
