c8151b89062acf0c147b97bfaf528a33dd3fa8fe
hamza <elbazhamza77@gmail.com>
2024-11-03 16:20:12 +0100

    building setup

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 3000;
-
-let fetch;
-import('node-fetch').then(({ default: _fetch }) => {
-    fetch = _fetch;
-});
 
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post('/form', async (req, res) => {
-    const { name, start, destinations, numbers, emails, messages } = req.body;
-
+    const { name, start, destinations, numbers, emails } = req.body;
+    
     try {
-        if (!fetch) {
-            // Import fetch if it's not already available
-            await import('node-fetch').then(({ default: _fetch }) => {
-                fetch = _fetch;
-            });
-        }
-
         const response = await fetch('/.netlify/functions/sendEmail', {
             method: 'POST',
             body: JSON.stringify({ name, start, destinations, numbers, emails, messages }),
@@ -32,6 +21,19 @@ app.post('/form', async (req, res) => {
         res.status(response.status).json(result);
     } catch (error) {
         console.error("Error:", error);
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.error("Response data:", error.response.data);
+            console.error("Response status:", error.response.status);
+            console.error("Response headers:", error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.error("Request data:", error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.error("Error message:", error.message);
+        }
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
